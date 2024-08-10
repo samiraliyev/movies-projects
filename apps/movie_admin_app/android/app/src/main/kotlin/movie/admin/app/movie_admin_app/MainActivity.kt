f2152e160b67fa87b2b425269497d523fac779cb
samiraliyev <samir.aliyev@code.edu.az>
2024-08-10 17:02:22 +0400

    main configuration
    melos, fvm

---
+++ apps/movie_admin_app/android/app/src/main/kotlin/movie/admin/app/movie_admin_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package movie.admin.app.movie_admin_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ apps/movie_user_app/android/app/src/main/kotlin/movie/user/app/movie_user_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package movie.user.app.movie_user_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
